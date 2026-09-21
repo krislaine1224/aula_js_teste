d8d59ffa556d34e577ce0efdc79e25c2259c6c0f
krislaine1224 <krislaine1224@github.com>
2026-09-21 20:09:01 -0300

    Adiciona arquivos do projeto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # aula_js_teste
+
+Projeto para aula de desenvolvimento web 2 sem 26 Piaget
---
+++ main.js
@@ -0,0 +1,24 @@
+
+var a = 5
+var b = 7
+var c = a+b
+console.log(c)
+
+var a = 3
+var b = 4
+
+function somar(x+y){
+    var c = x+y
+    return c
+}
+
+console.log(soma(a,b))
+
+var a = 0
+let b = 0
+const d = 0
+
+function somar(x+y){
+    let c = x+y
+    return c
+}
